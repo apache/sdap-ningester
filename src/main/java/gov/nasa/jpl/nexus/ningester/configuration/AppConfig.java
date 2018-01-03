af4467367ea9fff792152189ea33314cb8012cc0
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-01-03 12:58:02 -0800

    trying out some new configuration properties

--- src/main/java/gov/nasa/jpl/nexus/ningester/configuration/AppConfig.java
+++ src/main/java/gov/nasa/jpl/nexus/ningester/configuration/AppConfig.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
