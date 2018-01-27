f4d5cee92fe703ef4efb4c16a3a73e6ccbe65f46
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-01-26 19:16:25 -0800

    launching ningesterpy process

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,6 @@ NINGESTER_JAR=`find ningester/build/libs -name ningester*.jar`
 CONFIG_FILES=`find /config -name "*.yml" | awk -vORS=, '{ print $1 }'`
 GRANULE=`find /data -type f`
 
-java -Dspring.profiles.active=$1 -Dspring.config.location=${CONFIG_FILES} -jar ${NINGESTER_JAR} granule=${GRANULE}
+python -m ningesterpy | sed "s/^/[ningesterpy] /" &
+
+java -Dspring.profiles.active=$1 -Dspring.config.location=${CONFIG_FILES} -jar ${NINGESTER_JAR} granule=${GRANULE} | sed "s/^/[ningester] /"
